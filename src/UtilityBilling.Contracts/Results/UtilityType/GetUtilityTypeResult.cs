afdac225f1d792d1c3708cbaa9ce65673f82944b
dz-techi <dainiszogots@gmail.com>
2025-08-06 23:35:24 +0300

    Get rid of utility type icon

--- src/UtilityBilling.Contracts/Results/UtilityType/GetUtilityTypeResult.cs
+++ src/UtilityBilling.Contracts/Results/UtilityType/GetUtilityTypeResult.cs
@@ -5,7 +5,6 @@ namespace UtilityBilling.Contracts.Results.UtilityType;
 public class GetUtilityTypeResult
 {
     public UtilityBillType Value { get; set; }
-    public string Icon { get; set; } = null!;
     public MeasurementUnitType UnitMeasurementType { get; set; }
 
     public static GetUtilityTypeResult FromEnum(UtilityBillType utilityBillType)
@@ -13,29 +12,7 @@ public class GetUtilityTypeResult
         return new GetUtilityTypeResult
         {
             Value = utilityBillType,
-            Icon = GetIcon(utilityBillType),
             UnitMeasurementType = GetMeasurementUnit(utilityBillType)
-        };
-    }
-
-    private static string GetIcon(UtilityBillType utilityBillType)
-    {
-        return utilityBillType switch
-        {
-            UtilityBillType.Electricity => "⚡",
-            UtilityBillType.Water => "💧",
-            UtilityBillType.Gas => "🔥",
-            UtilityBillType.Sewer => "🚰",
-            UtilityBillType.TrashAndRecycling => "🗑️",
-            UtilityBillType.Heating => "🔥",
-            UtilityBillType.Internet => "🌐",
-            UtilityBillType.CableOrSatelliteTv => "📺",
-            UtilityBillType.Telephone => "📞",
-            UtilityBillType.SecuritySystem => "🔒",
-            UtilityBillType.Maintenance => "🔧",
-            UtilityBillType.RentalFee => "💰",
-            UtilityBillType.DrinkingWater => "🚰",
-            _ => "📋"
         };
     }
 
